ca9b899fda4d101d6cb5eb120490c79a0c796ff0
Priya Bhatia <priya.bhatia6971@gmail.com>
2025-09-26 20:38:11 +0530

    Updated Templates

--- app.py
+++ app.py
@@ -12,13 +12,17 @@ def home():
     return render_template('home.html')
 
 @app.route('/predict_api', methods=['POST'])
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
-    data = request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1, -1))
-    new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
+    # Use request.form to get data from the HTML form
+    data = [float(x) for x in request.form.values()]
+    
+    # The rest of your code remains the same
+    # The data needs to be reshaped for prediction
+    final_input = np.array(data).reshape(1, -1)
+    new_data = scaler.transform(final_input)
     output = model.predict(new_data)
-    print(output[0])
+    
     return jsonify(output[0])
 
 if __name__ == "__main__":
